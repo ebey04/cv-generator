762c36a4ee07c3b37522c9cd1e0a09f2561e471b
Elizabeth Bey <ebey04@gmail.com>
2025-12-23 08:59:38 -0500

    Cleanup given folders and build Form.jsx

---
+++ src/Form/Form.jsx
@@ -0,0 +1,23 @@
+function Input({
+    label,
+    type = "text",
+    value,
+    onChange,
+    name,
+    placeholder
+    }) 
+    {
+        return (
+            <div className="input-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                id={name}
+                name={name}
+                type={type}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+            />
+            </div>
+        );
+    }
