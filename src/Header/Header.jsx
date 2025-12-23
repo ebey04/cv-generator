3cb3309e9a886cb51609a44e8f145644f41646a6
Elizabeth Bey <ebey04@gmail.com>
2025-12-23 09:58:13 -0500

    feat: add Header.jsx and style. Create :root for custom colors

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() (
+    {
+        return {
+            <>
+                <h1>Resume Generator</h1>
+                <p>Use the input fields to the left and click the submit button to see a professional resume.</p>
+            </>
+        }
+    }
+)
